4535c98eaf6812ade2770a4f323951b092228840
Artyem Savchenko <a.savchenko89@yandex.ru>
2022-09-05 11:06:54 +0300

    feat: Сверстал мобильную шапку

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,27 +1,45 @@
+import { useState } from 'react';
 import { Link, Route, Switch } from 'react-router-dom';
 import logo from '../images/logo.svg';
 
 export default function Header({ onSignOut, userEmail }) {
+  const [isOpenedMenu, setIsOpenedMenu] = useState(false);
+
+  const handlerMenuBtnClick = () => {
+    if (isOpenedMenu) {
+      setIsOpenedMenu(false);
+    } else {
+      setIsOpenedMenu(true);
+    }
+  }
 
   return (
-    <header className="header page__section">
-      <img src={logo} alt="Логотип Место Россия." className="logo" />
+    <header className="header">
+      <img src={logo} alt="Логотип Место Россия." className="logo header__logo" />
       <Switch>
         <Route exact path="/">
-          <div className="header__user-info-box">
+          <div className={`header__user-info-box ${isOpenedMenu ? "header__user-info-box_opened" : ""}`}>
             <p className="header__text">{userEmail}</p>
             <button onClick={onSignOut} type="button" className="header__link">
               Выйти
             </button>
           </div>
+          <button
+            type="button"
+            className={`burger-btn header__burger-btn ${isOpenedMenu ? "burger-btn_opened-menu" : ""}`}
+            aria-label="Кнопка меню."
+            onClick={handlerMenuBtnClick}
+          >
+            <div className="burger-btn__cross"></div>
+          </button>
         </Route>
         <Route path="/sign-in">
-          <Link to="/sign-up" className="header__link">
+          <Link to="/sign-up" className="header__link header__link_right">
             Регистрация
           </Link>
         </Route>
         <Route path="/sign-up">
-          <Link to="/sign-in" className="header__link">
+          <Link to="/sign-in" className="header__link header__link_right">
             Войти
           </Link>
         </Route>
