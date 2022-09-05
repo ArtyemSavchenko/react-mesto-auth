7eaad26d0f99176ce8976947a24fe4e71d610518
Artyem Savchenko <a.savchenko89@yandex.ru>
2022-09-05 16:48:44 +0300

    refactor: Реализовал все попапы через компонент Popup теперь все попапы обернуты одним компонентом с логикой закрытия и открытия

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -1,49 +1,33 @@
+import Popup from './Popup';
+
 export default function PopupWithForm({
   name,
   title,
   children,
   btnText,
-  onClose,
-  isOpen,
   onSubmit,
-  isLoading
+  isLoading,
+  ...restProps
 }) {
-  const handlerOverlayClick = e => {
-    if (e.target === e.currentTarget) onClose();
-  };
-
   return (
-    <div
-      className={`popup popup_type_${name} popup_type_form ${
-        isOpen && 'popup_active'
-      }`}
-      onClick={handlerOverlayClick}
-    >
-      <div className="popup__window">
-        <h3 className="popup__title">{title}</h3>
-        <form
-          className="popup__form"
-          name={name}
-          autoComplete="off"
-          // noValidate
-          onSubmit={onSubmit}
-        >
-          {children}
-          <button
-            className="popup__submit-btn"
-            type="submit"
-            disabled={isLoading}
-          >
-            {btnText}
-          </button>
-        </form>
+    <Popup window {...restProps}>
+      <h3 className="popup__title">{title}</h3>
+      <form
+        className="popup__form"
+        name={name}
+        autoComplete="off"
+        // noValidate
+        onSubmit={onSubmit}
+      >
+        {children}
         <button
-          className="popup__close-btn"
-          type="button"
-          aria-label="Закрыть форму."
-          onClick={onClose}
-        ></button>
-      </div>
-    </div>
+          className="popup__submit-btn"
+          type="submit"
+          disabled={isLoading}
+        >
+          {btnText}
+        </button>
+      </form>
+    </Popup>
   );
 }
