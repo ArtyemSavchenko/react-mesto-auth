c17eaee5964366e7aae69d0ac11d64e17af05777
Artyem Savchenko <a.savchenko89@yandex.ru>
2022-09-05 15:59:08 +0300

    refactor: Почистил код

--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -2,7 +2,7 @@ import Popup from './Popup';
 
 export default function ImagePopup({ card, onClose }) {
   return (
-    <Popup type="image" isOpen={Object.keys(card).length} onClose={onClose}>
+    <Popup isOpen={Object.keys(card).length} onClose={onClose}>
         <figure className="card-image__box">
           <img className="card-image__img" src={card.link} alt={card.name} />
           <figcaption className="card-image__caption">{card.name}</figcaption>
