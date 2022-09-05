733e406fdee75c791d084f0177e12cfbe5301e0f
Artyem Savchenko <a.savchenko89@yandex.ru>
2022-09-05 15:59:53 +0300

    refactor: Заменил в хэдере свитч на хук useLocation

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,10 @@
 import { useState } from 'react';
-import { Link, Route, Switch } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import logo from '../images/logo.svg';
 
 export default function Header({ onSignOut, userEmail }) {
   const [isOpenedMenu, setIsOpenedMenu] = useState(false);
+  const location = useLocation();
 
   const handlerMenuBtnClick = () => {
     if (isOpenedMenu) {
@@ -11,14 +12,22 @@ export default function Header({ onSignOut, userEmail }) {
     } else {
       setIsOpenedMenu(true);
     }
-  }
+  };
 
   return (
     <header className="header">
-      <img src={logo} alt="Логотип Место Россия." className="logo header__logo" />
-      <Switch>
-        <Route exact path="/">
-          <div className={`header__user-info-box ${isOpenedMenu ? "header__user-info-box_opened" : ""}`}>
+      <img
+        src={logo}
+        alt="Логотип Место Россия."
+        className="logo header__logo"
+      />
+      {location.pathname === '/' && (
+        <>
+          <div
+            className={`header__user-info-box ${
+              isOpenedMenu ? 'header__user-info-box_opened' : ''
+            }`}
+          >
             <p className="header__text">{userEmail}</p>
             <button onClick={onSignOut} type="button" className="header__link">
               Выйти
@@ -26,24 +35,26 @@ export default function Header({ onSignOut, userEmail }) {
           </div>
           <button
             type="button"
-            className={`burger-btn header__burger-btn ${isOpenedMenu ? "burger-btn_opened-menu" : ""}`}
+            className={`burger-btn header__burger-btn ${
+              isOpenedMenu ? 'burger-btn_opened-menu' : ''
+            }`}
             aria-label="Кнопка меню."
             onClick={handlerMenuBtnClick}
           >
-            <div className="burger-btn__cross"></div>
+            <div className="burger-btn__cross"/>
           </button>
-        </Route>
-        <Route path="/sign-in">
-          <Link to="/sign-up" className="header__link header__link_right">
-            Регистрация
-          </Link>
-        </Route>
-        <Route path="/sign-up">
-          <Link to="/sign-in" className="header__link header__link_right">
-            Войти
-          </Link>
-        </Route>
-      </Switch>
+        </>
+      )}
+      {location.pathname === '/sign-in' && (
+        <Link to="/sign-up" className="header__link header__link_right">
+          Регистрация
+        </Link>
+      )}
+      {location.pathname === '/sign-up' && (
+        <Link to="/sign-in" className="header__link header__link_right">
+          Войти
+        </Link>
+      )}
     </header>
   );
 }
